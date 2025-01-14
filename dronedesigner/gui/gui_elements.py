aa9209fbd48db9b2d172ed0abcaa53ed549da4de
b-r-y <87ow9zlxp@relay.firefox.com>
2025-01-14 23:11:28 +0100

    added cost display and refactored components callbacks

--- dronedesigner/gui/gui_elements.py
+++ dronedesigner/gui/gui_elements.py
@@ -1,3 +1,5 @@
+from functools import partial
+
 from ipywidgets import Box, Dropdown, Label, Layout, Text
 
 from dronedesigner.components.drone import Drone
@@ -15,32 +17,14 @@ def update_mass_display() -> None:
 
 
 def update_cost_display() -> None:
-    pass
+    cost_value.value = str(drone.cost())
 
 
-def update_frame(name) -> None:
-    drone.frame = load_component(filename="frames", component_name=name.new)
-    update_mass_display()
-    update_cost_display()
-
-
-def update_stack(name) -> None:
-    drone.stack = load_component(filename="stacks", component_name=name.new)
-    update_mass_display()
-    update_cost_display()
-
-
-def update_receiver(name) -> None:
-    drone.receiver = load_component(
-        filename="receivers", component_name=name.new
+def update_drone(component: str, source_file: str, value) -> None:
+    new_component = load_component(
+        filename=source_file, component_name=value.new
     )
-    update_mass_display()
-    update_cost_display()
-
-
-def update_motor(name) -> None:
-    drone.motor = load_component(filename="motors", component_name=name.new)
-
+    setattr(drone, component, new_component)
     update_mass_display()
     update_cost_display()
 
@@ -60,7 +44,9 @@ frames_w = Dropdown(
     value="Default Frame",
     description="Frame: ",
 )
-frames_w.observe(handler=update_frame, names="value")
+frames_w.observe(
+    handler=partial(update_drone, "frame", "frames"), names="value"
+)
 
 
 stacks_w = Dropdown(
@@ -68,7 +54,9 @@ stacks_w = Dropdown(
     value="Default Stack",
     description="Stack: ",
 )
-stacks_w.observe(handler=update_stack, names="value")
+stacks_w.observe(
+    handler=partial(update_drone, "stack", "stacks"), names="value"
+)
 
 receivers_w = Dropdown(
     options=list_components(
@@ -77,14 +65,18 @@ receivers_w = Dropdown(
     value="Default Receiver",
     description="Receiver: ",
 )
-receivers_w.observe(handler=update_receiver, names="value")
+receivers_w.observe(
+    handler=partial(update_drone, "receiver", "receivers"), names="value"
+)
 
 motors_w = Dropdown(
     options=list_components(objects_list=load_components(filename="motors")),
     value="Default Motor",
     description="Motor: ",
 )
-motors_w.observe(handler=update_motor, names="value")
+motors_w.observe(
+    handler=partial(update_drone, "motor", "motors"), names="value"
+)
 
 motor_count_w = Dropdown(
     options=["1", "2", "3", "4"],
@@ -125,12 +117,26 @@ mass_value: Text = Text(
     description="",
 )
 
+cost_value: Text = Text(
+    value="0",
+    placeholder="This is the total calculated cost of the drone",
+    description="",
+)
+
 outputs: list[Box] = [
     Box(
         children=[
             Label(value="Calculated mass: "),
             mass_value,
             Label(value="  [grams]"),
+        ],
+        layout=form_item_layout,
+    ),
+    Box(
+        children=[
+            Label(value="Calculated cost: "),
+            cost_value,
+            Label(value="  [DKK]"),
         ],
         layout=form_item_layout,
     ),
