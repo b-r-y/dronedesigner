52f5ef51fb8779fb953ea20d7e89df774e05a622
b-r-y <87ow9zlxp@relay.firefox.com>
2025-01-15 23:29:02 +0100

    gui updates and added more components to the gui

--- dronedesigner/gui/gui_elements.py
+++ dronedesigner/gui/gui_elements.py
@@ -25,34 +25,35 @@ def update_drone(component: str, source_file: str, value) -> None:
         filename=source_file, component_name=value.new
     )
     setattr(drone, component, new_component)
-    update_mass_display()
-    update_cost_display()
-
-
-def update_motor_count(name) -> None:
-    drone.motor.number_of_units = int(name.new)
+    if component == "motor":
+        drone.motor.number_of_units = 4
+    if component == "propeller":
+        drone.propeller.number_of_units = 4
+    if component == "battery_strap":
+        drone.battery_strap.number_of_units = 2
     update_mass_display()
     update_cost_display()
 
 
 form_item_layout = Layout(
-    display="flex", flex_flow="row", justify_content="space-between"
+    display="flex",
+    flex_flow="row",
+    justify_content="space-between",
 )
 
 frames_w = Dropdown(
     options=list_components(objects_list=load_components(filename="frames")),
     value="Default Frame",
-    description="Frame: ",
+    description="",
 )
 frames_w.observe(
     handler=partial(update_drone, "frame", "frames"), names="value"
 )
 
-
 stacks_w = Dropdown(
     options=list_components(objects_list=load_components(filename="stacks")),
     value="Default Stack",
-    description="Stack: ",
+    description="",
 )
 stacks_w.observe(
     handler=partial(update_drone, "stack", "stacks"), names="value"
@@ -63,52 +64,114 @@ receivers_w = Dropdown(
         objects_list=load_components(filename="receivers")
     ),
     value="Default Receiver",
-    description="Receiver: ",
+    description="",
 )
 receivers_w.observe(
     handler=partial(update_drone, "receiver", "receivers"), names="value"
 )
 
+receiver_antennas_w = Dropdown(
+    options=list_components(
+        objects_list=load_components(filename="receiver_antennas")
+    ),
+    value="Default Receiver Antenna",
+    description="",
+)
+receiver_antennas_w.observe(
+    handler=partial(update_drone, "receiver_antenna", "receiver_antennas"),
+    names="value",
+)
+
 motors_w = Dropdown(
     options=list_components(objects_list=load_components(filename="motors")),
     value="Default Motor",
-    description="Motor: ",
+    description="",
 )
 motors_w.observe(
     handler=partial(update_drone, "motor", "motors"), names="value"
 )
 
-motor_count_w = Dropdown(
-    options=["1", "2", "3", "4"],
-    value="4",
-    description="No: ",
+propellers_w = Dropdown(
+    options=list_components(
+        objects_list=load_components(filename="propellers")
+    ),
+    value="Default Properller",
+    description="",
 )
-motor_count_w.observe(handler=update_motor_count, names="value")
+propellers_w.observe(
+    handler=partial(update_drone, "propeller", "propellers"), names="value"
+)
+
+batteries_w = Dropdown(
+    options=list_components(
+        objects_list=load_components(filename="batteries")
+    ),
+    value="Default Battery",
+    description="",
+)
+batteries_w.observe(
+    handler=partial(update_drone, "battery", "batteries"), names="value"
+)
+
+battery_straps_w = Dropdown(
+    options=list_components(
+        objects_list=load_components(filename="battery_straps")
+    ),
+    value="Default Battery Strap",
+    description="",
+)
+battery_straps_w.observe(
+    handler=partial(update_drone, "battery_strap", "battery_straps"),
+    names="value",
+)
+
+vtx_kits_w = Dropdown(
+    options=list_components(objects_list=load_components(filename="vtx_kits")),
+    value="Default VTX Kit",
+    description="",
+)
+vtx_kits_w.observe(
+    handler=partial(update_drone, "vtx", "vtx_kits"), names="value"
+)
 
 selections: list[Box] = [
+    Box(children=[Label(value="Drone:")], layout=form_item_layout),
     Box(
-        children=[Label(value="Drone:")],
+        children=[Label(value="Battery: "), batteries_w],
         layout=form_item_layout,
     ),
     Box(
-        children=[
-            frames_w,
-        ],
+        children=[Label(value="Battery Straps (x2): "), battery_straps_w],
         layout=form_item_layout,
     ),
     Box(
-        children=[
-            stacks_w,
-        ],
+        children=[Label(value="Frame: "), frames_w],
         layout=form_item_layout,
     ),
     Box(
-        children=[
-            receivers_w,
-        ],
+        children=[Label(value="Stack: "), stacks_w],
         layout=form_item_layout,
     ),
-    Box(children=[motors_w, motor_count_w], layout=form_item_layout),
+    Box(
+        children=[Label(value="Receiver: "), receivers_w],
+        layout=form_item_layout,
+    ),
+    Box(
+        children=[Label(value="Receiver Antenna: "), receiver_antennas_w],
+        layout=form_item_layout,
+    ),
+    Box(
+        children=[Label(value="Motor: "), motors_w],
+        layout=form_item_layout,
+    ),
+    Box(
+        children=[Label(value="Properller: "), propellers_w],
+        layout=form_item_layout,
+    ),
+    Box(
+        children=[Label(value="VTX Kit: "), vtx_kits_w],
+        layout=form_item_layout,
+    ),
 ]
 
 mass_value: Text = Text(
@@ -126,17 +189,15 @@ cost_value: Text = Text(
 outputs: list[Box] = [
     Box(
         children=[
-            Label(value="Calculated mass: "),
+            Label(value="Calculated mass [gram]: "),
             mass_value,
-            Label(value="  [grams]"),
         ],
         layout=form_item_layout,
     ),
     Box(
         children=[
-            Label(value="Calculated cost: "),
+            Label(value="Calculated cost [DKK]: "),
             cost_value,
-            Label(value="  [DKK]"),
         ],
         layout=form_item_layout,
     ),
